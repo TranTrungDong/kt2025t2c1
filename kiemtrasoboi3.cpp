329abfdada258970b7279226cac8be0438c2dd33
Van Giap <hoanggiap2k4@gmail.com>
2025-11-17 08:18:43 +0700

    update

---
+++ kiemtrasoboi3.cpp
@@ -0,0 +1,12 @@
+#includes <iostream>
+using namespace std;
+
+int main(){
+	int n=0;
+	cout<<"Nhap n";
+	cin>>"Nhap n";
+	if(n%3==0)
+		cout<<"Day la so boi 3";
+	else
+		cout<<"Day khong phai la boi 3";
+}
