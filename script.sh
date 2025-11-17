667ebd6fb302db7a55f9fbceab64d1dcb9590ead
MQUY54-git <accpesquy1245@gmail.com>
2025-11-17 08:02:12 +0700

    Hoan thanh cau 2 shell script kiem tra boi so cua 3

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+checkBoiCua3() {
+	local n=$1
+	if (( n % 3 == 0 )); then
+		echo "$n la boi so cua 3"
+		return
+	else
+		echo "$n khong la boi so cua 3"
+	fi
+}
+
+read n
+
+checkBoiCua3 30
+
