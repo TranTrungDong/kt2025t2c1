e1dec24e94e203f91d34df9e0f24230292b47d64
MQUY54-git <accpesquy1245@gmail.com>
2025-11-17 07:56:12 +0700

    Hoan thanh cau 2 kiem tra boi so cua 3 bang c++

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int n =0;
+	cout <<"nhap vao so nguyen n: ";
+	cin>>n;
+	if(n % 3==0) { 
+		cout << n << " la boi so cua 3";
+	}
+	else 
+		cout << n << " khong la boi cua 3";
+	return 0;
+}
